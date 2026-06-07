ad3dd351f22029c32655ac4e4c7ab77540e4aefd
JasonLau <jasonlau@shimano.com.sg>
2026-06-07 10:58:20 +0800

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+  base: '/autopilot-sop/',
   plugins: [react()],
   server: {
     proxy: {
